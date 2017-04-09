598095e58ea26fc15ab0d12389f14992063be387
Anshuman Patnaik <anshumanec11@gmail.com>
2017-04-09 10:28:35 +0530

    cache matrix code updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an assignment submission for Coursera R-Programming Module.
+##Under this assignment a caching solution is to be constructed. The sample function is provided with 
+#mean calculation as example.
 
-## Write a short comment describing this function
+
+## The function makeCacheMatrix created for Setting and getting the Inverse vector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+## second part of function will Cache solve the multiple iterations
+## I have also tested this function with Sys.time (this code is commented)
 
-## Write a short comment describing this function
-
+# Assumption that the matrix is always invertable
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    return(inv) 
+    message("This function run is on Cache data")
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+        
+## Function test
+# x = rbind(c(2, 4), c(4, 2))
+#m = makeCacheMatrix(x)
+#m$get()
+
+
+#cacheSolve(m)
+#first_time=Sys.time()
+
+
+#cacheSolve(m)
+#second_time=Sys.time()
+#cacheSolve(m)
+#third_time=Sys.time()
+#elapse_time <-second_time- first_time
+#elapse_time_next <-third_time- second_time
+#print(elapse_time)
+#print(elapse_time_next)
+
+#result
+
+#print(elapse_time)
+#Time difference of 0.0009999275 secs
+# print(elapse_time_next)
+#Time difference of 0 secs
